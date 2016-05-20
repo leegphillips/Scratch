623763ae4ac809c8d0439dd0f8f2ccb0d42cad4e
CDG <cdg@Unknown-b8-8d-12-15-28-c4.home>
2016-05-20 18:11:46 +0100

    Added File PoC

---
+++ FileUtils/src/main/java/com/github/leegphillips/fileUtils/App.java
@@ -0,0 +1,35 @@
+package com.github.leegphillips.fileUtils;
+
+import java.io.*;
+import java.util.stream.IntStream;
+
+public class App
+{
+    private static final int MAX = 100000;
+
+    public static void main( String[] args ) throws IOException, InterruptedException {
+        int[] ints = IntStream.iterate(1, i -> i + 1).limit(MAX).toArray();
+
+        File file = File.createTempFile(Long.toString(System.currentTimeMillis()), ".tmp");
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+            try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream)) {
+                try (DataOutputStream dos = new DataOutputStream(bufferedOutputStream)) {
+                    for (int i = 0; i < ints.length; i++) {
+                        dos.writeInt(ints[i]);
+                    }
+                }
+            }
+        }
+
+        try (FileInputStream fis = new FileInputStream(file)) {
+            try (BufferedInputStream bis = new BufferedInputStream(fis)) {
+                try (DataInputStream dis = new DataInputStream(bis)) {
+                    for (int value = dis.readInt(); true; value = dis.readInt()) {
+                        System.out.println(value);
+                    }
+                }
+            }
+        }
+    }
+}
